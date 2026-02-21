d2efdcaefe4d8e042c80a535bcf4c605a22763e5
bichitrabehera <bichitrabehera.amcec@gmail.com>
2026-02-21 11:23:41 +0530

    app file changed

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("server running for skribbl clone ");
+  res.send("server running for skribbl clone yahhhh ");
 });
 
 export default app;
